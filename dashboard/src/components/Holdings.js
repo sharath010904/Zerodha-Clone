d0ca2bf8ee7f3246f2ad758470b00b804c223587
Sharath Mandapati <sharathchandra010904@gmail.com>
2026-09-08 00:48:43 +0530

    Update API endpoint for fetching all holdings

--- dashboard/src/components/Holdings.js
+++ dashboard/src/components/Holdings.js
@@ -8,7 +8,7 @@ const Holdings = () => {
   const [allHoldings, setAllHoldings] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:3002/allHoldings").then((res) => {
+   axios.get("https://zerodha-clone-m2r0.onrender.com/allHoldings").then((res) => {
       // console.log(res.data);
       setAllHoldings(res.data);
     });
