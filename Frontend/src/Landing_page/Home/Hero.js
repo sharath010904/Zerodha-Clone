1c952bcd8dde7b07a8b3595ceb2b90572ae8fe59
Sharath Mandapati <sharathchandra010904@gmail.com>
2026-09-09 22:47:19 +0530

    Replace button with anchor link for signup

--- Frontend/src/Landing_page/Home/Hero.js
+++ Frontend/src/Landing_page/Home/Hero.js
@@ -14,12 +14,17 @@ function Hero() {
           Online platform to invest in stocks, derivatives, mutual funds, and
           more
         </p>
-        <button
-          className="p-2 btn btn-primary fs-5 mb-5"
-          style={{ width: "20%", margin: "0 auto" }}
-        >
-          Signup Now
-        </button>
+<a
+  href="https://zerodha-clone-1-et6x.onrender.com/"
+  className="p-2 btn btn-primary fs-5 mb-5"
+  style={{
+    width: "20%",
+    margin: "0 auto",
+    textDecoration: "none",
+  }}
+>
+  Signup Now
+</a>
       </div>
     </div>
   );
